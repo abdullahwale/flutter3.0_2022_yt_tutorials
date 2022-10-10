24cb692600466134304d17d36f5671b972f0a448
Muhammad Abdullah <39946504+abdullahwale@users.noreply.github.com>
2022-10-10 09:51:00 +0500

    sec A B20 Dice App

--- diceapp_seca_b18/lib/main.dart
+++ diceapp_seca_b18/lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-
-Random random = new Random();
+Random random= new Random();
 
 void main() {
-  runApp(DiceAppSecAB18());
+  runApp(MyApp());
 }
+//Statless Widgets
+class MyApp extends StatelessWidget {
 
-class DiceAppSecAB18 extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Dice App 2022"),
+          title: Text("Dice App"),
         ),
         body: Dice(),
       ),
@@ -21,84 +23,122 @@ class DiceAppSecAB18 extends StatelessWidget {
   }
 }
 
+
+//Statfull Widgets
 class Dice extends StatefulWidget {
   const Dice({Key? key}) : super(key: key);
 
   @override
   State<Dice> createState() => _DiceState();
 }
-
 class _DiceState extends State<Dice> {
-  int num = 1;
-  int num2=1;
 
+  int num=1;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: TextButton(
-            child: Container(
-              color: Colors.greenAccent,
-              child: Center(
-                child: Image(
-                  image: AssetImage("images/dice$num.png"),
+    return Column(children: [
+      Expanded(
+        child: Row(children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                //Call Back Function, Anonymous Function,Nameless Funtion
+                setState(() {
+                  num= random.nextInt(6)+1;
+                });
+                print(num);
+              },
+              child: Container(
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Image(
+                    image: AssetImage("images/dice$num.png"),
+                  ),
                 ),
               ),
             ),
-            onPressed: () {
-              setState(() {
-                num = random.nextInt(6)+1;
-              });
-              print(num);
-            },
           ),
-        ),
-        Expanded(
-          child: TextButton(
-            child: Container(
-              color: Colors.greenAccent,
-              child: Center(
-                child: Image(
-                  image: AssetImage("images/dice$num2.png"),
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                //Call Back Function, Anonymous Function,Nameless Funtion
+                setState(() {
+                  num= random.nextInt(6)+1;
+                });
+                print(num);
+              },
+              child: Container(
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Image(
+                    image: AssetImage("images/dice$num.png"),
+                  ),
                 ),
               ),
             ),
-            onPressed: () {
-              setState(() {
-                num2 = random.nextInt(6)+1;
-              });
-              print(num2);
-            },
+          )
+        ],),
+      ),
+      Expanded(
+        child: Row(children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                //Call Back Function, Anonymous Function,Nameless Funtion
+                setState(() {
+                  num= random.nextInt(6)+1;
+                });
+                print(num);
+              },
+              child: Container(
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Image(
+                    image: AssetImage("images/dice$num.png"),
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
-    );
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                //Call Back Function, Anonymous Function,Nameless Funtion
+                setState(() {
+                  num= random.nextInt(6)+1;
+                });
+                print(num);
+              },
+              child: Container(
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Image(
+                    image: AssetImage("images/dice$num.png"),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],),
+      )
+    ],);
   }
 }
 
 /*
-         if(num==1)
-            num=2;
-          else if (num==2)
-            num=3;
-          else if (num==3)
-            num=4;
-          else if (num==4)
-            num=5;
-          else if (num==5)
-            num=6;
-          else
-            num=1;
+ // if (num == 1)
+          //   num = 2;
+          // else if (num == 2)
+          //   num=3;
+          // else if (num == 3)
+          //   num=4;
+          // else if (num == 4)
+          //   num=5;
+          // else if (num==5)
+          //   num=6;
+          // else
+          //   num=1;
  */
-
-/*
-Container(
-          color: Colors.greenAccent,
-          child: Center(
-            child: Image(
-              image: AssetImage("images/dice1.png"),
-            ),
-          ),
-        )
- */
+// void fun(){
+//   print("Clicked");
+// }
